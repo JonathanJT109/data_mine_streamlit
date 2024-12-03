a12a8f217dce940ebd80f98fce2b3f1c7cda1266
Jonathan Gonzalez <jonathangon.2014@gmail.com>
2024-12-02 19:40:00 -0500

    I don't know anymore

--- main.py
+++ main.py
@@ -3,69 +3,21 @@ import pandas as pd
 import streamlit as st
 
 from server import (
-    get_counties,
-    get_county_house_value,
-    get_county_rent_rates,
-    get_county_employment,
+    get_county_info,
+    get_counties, get_percent_change
 )
 
 indiana_counties = get_counties("IN")
 indiana_counties.sort()
 
 
-def get_test(state: str, county: str):
-    data = {}
-    hv = get_county_house_value(state, county)
-    rr = get_county_rent_rates(state, county)
-    _, pi, p, te, ws = get_county_employment(state, county)
-    # Temporal fix
-    if hv is not None:
-        hv["Prediction"] = False
-        hv.loc[hv.index[-10:], "Prediction"] = True
-    if rr is not None:
-        rr["Prediction"] = False
-        rr.loc[rr.index[-10:], "Prediction"] = True
-    data["House Value"] = hv
-    data["Rent Rate"] = rr
-    data["Personal Income"] = pi
-    data["Population"] = p
-    data["Total Employment"] = te
-    data["Wages and Salaries"] = ws
+def get_info(state: str, county: str):
+    data = get_county_info(state, county)
     return data
 
 
-def simulate_model_data(location):
-    num_rows = 1
-    data = {
-        "house_value": np.random.randint(100000, 500000, size=num_rows),
-        "rent_value": np.random.randint(800, 3000, size=num_rows),
-        "crime_rate": np.random.uniform(0, 10, size=num_rows),
-        "vacancy_rate": np.random.uniform(0, 1, size=num_rows),
-    }
-
-    df = pd.DataFrame(data)
-    return df
-
-
-@st.cache_data
-def data():
-    df = pd.DataFrame(
-        (np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4]),
-        columns=["lat", "lon"],
-    )
-    return df
-
-
-def send_data(location):
-    try:
-        data = simulate_model_data(location)
-        st.write("Data sent")
-        return data
-    except:
-        st.write("Error sending data")
-
-
 def app():
+    st.title("County Market Information")
     location = st.selectbox(
         "Select the location",
         indiana_counties,
@@ -75,34 +27,27 @@ def app():
     search_button = st.button("Search")
     warn = st.empty()
 
-    # QUESTION: Use google maps instead?
-    # st.map(data())
+    # QUESTION: Use Google Maps instead?
+    st.map()
 
     if search_button:
         if location is not None:
-            predictions = send_data(location)
-            if predictions is not None:
-                house_price = f"${predictions['house_value'][0]:.2f}"
-                rent_value = f"${predictions['rent_value'][0]:.2f}"
-                crime_rate = f"{predictions['crime_rate'][0]:.2f}%"
-                vacancy_rate = f"{predictions['crime_rate'][0]:.2f}%"
-                col1, col2, col3, col4 = st.columns(4)
-                col1.metric("House Value", house_price, "4%")
-                col2.metric("Rent Rates", rent_value, "10%")
-                col3.metric("Crime Rate", crime_rate, "-14%")
-                col4.metric("Vacancy Rate", vacancy_rate, "-17%")
+            county_data = get_info("IN", location)
+            metrics = get_percent_change(county_data)
+            for key, metric in metrics.items():
+                name = key.upper().replace('_', ' ')
+                current, change = metric
+                st.metric(name, f"{current:.2f}", f"{change:.2f}%")
 
-            county_data = get_test("IN", location)
+            st.header("Change of Metrics through Time")
             for key, value in county_data.items():
-                print(value)
                 if value is not None:
                     st.subheader(f"{key} in {location}")
-                    # st.line_chart(
-                    #     value,
-                    #     y=["Value"],
-                    #     y_label=key,
-                    #     x_label="Date",
-                    # )
+                    st.line_chart(
+                        value,
+                        y_label=key,
+                        x_label="Date",
+                    )
                 else:
                     st.error(f"No {key} data found for this county.")
         else:
--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 import json
 import os
+import pickle
 
 import pandas as pd
 
@@ -114,7 +115,7 @@ def get_county_house_value(state: str, county: str):
     # Rename the index for clarity
     county_house_value.index = ["Value"]
 
-    return county_house_value.T
+    return county_house_value.T["Value"]
 
 
 def get_county_rent_rates(state: str, county: str):
@@ -138,7 +139,7 @@ def get_county_rent_rates(state: str, county: str):
     # Filter the data for the specified county
     county_rent_rate = state_rent_rate.loc[
         (state_rent_rate["State"] == state) & (state_rent_rate["RegionName"] == county)
-    ]
+        ]
 
     # If no data is found for the county, return None
     if county_rent_rate.shape[0] == 0:
@@ -164,12 +165,29 @@ def get_county_rent_rates(state: str, county: str):
     # Rename the index for clarity
     county_rent_rate.index = ["Value"]
 
-    return county_rent_rate.T
+    return county_rent_rate.T["Value"]
 
 
 def get_county_employment(state: str, county: str):
+    """
+    Retrieves employment data for a given county and state.
+
+    This function pulls employment data from a state-level dataset and filters it
+    for the specific county. The data is then cleaned by setting the 'Year' column
+    as the index and dropping unnecessary columns. The function returns a list of
+    DataFrames, each representing a different employment metric over time.
+
+    Args:
+        state (str): The name of the state (e.g., "California").
+        county (str): The name of the county (e.g., "Los Angeles").
+
+    Returns:
+        list of pd.DataFrame: A list where each item is a DataFrame representing
+                               a specific employment metric, with 'Year' and 'Value' columns.
+                               If no data is available for the county, returns None.
+    """
     state_employment = get_employment(state)
-    geo = f"{county.split()[0]}, {state}"
+    geo = f"{county.split()[0]}, {state}"  # Assuming the first word of the county name is sufficient for matching
     county_employment = state_employment.loc[state_employment["GeoName"] == geo]
     county_employment.set_index("Year", inplace=True)
     county_employment = county_employment.drop("GeoName", axis=1)
@@ -178,18 +196,124 @@ def get_county_employment(state: str, county: str):
     if county_employment.shape[0] == 0:
         return None
 
+    county_employment.index = pd.to_datetime(county_employment.index.astype(str), format='%Y')
+
     employment = []
     for col in county_employment.columns:
         metric = county_employment[col]
-        metric.columns = ["Year", "Value"]
+        metric.columns = ["Year", "Value"]  # Renaming columns for clarity
         employment.append(metric)
 
     return employment
 
 
+def open_model(name: str):
+    """
+    Opens a pre-trained model from a specified file.
+
+    This function loads a pickle file containing a machine learning model or any other
+    serialized object from disk. The model is expected to be located in the 'models' directory
+    and is named according to the format 'model_{name}.pkl'.
+
+    Args:
+        name (str): The name of the model to be loaded (e.g., "housing_prediction").
+
+    Returns:
+        object: The deserialized model object.
+    """
+    path = os.path.join(os.path.dirname(__file__), f"models/model_{name}.pkl")
+    with open(path, "rb") as f:
+        data = pickle.load(f)
+        return data
+
+
+def get_county_info(state: str, county: str):
+    """
+    Retrieves a set of key county-level data metrics for a given state and county.
+
+    This function combines data on various aspects of a county's economy, including house value,
+    rent rates, employment, personal income, population, and wages and salaries. Each data type
+    is retrieved by calling its respective function and is stored in a dictionary.
+
+    Args:
+        state (str): The name of the state (e.g., "California").
+        county (str): The name of the county (e.g., "Los Angeles").
+
+    Returns:
+        dict: A dictionary containing the following metrics:
+            - "House Value"
+            - "Rent Rate"
+            - "Personal Income"
+            - "Population"
+            - "Total Employment"
+            - "Wages and Salaries"
+          Each metric is associated with its corresponding data.
+    """
+    county_data = {}
+    hv = get_county_house_value(state, county)
+    rr = get_county_rent_rates(state, county)
+    ind, pi, p, te, ws = get_county_employment(state, county)
+
+    county_data["House Value"] = hv
+    county_data["Rent Rate"] = rr
+    county_data["Personal Income"] = pi
+    county_data["Population"] = p
+    county_data["Total Employment"] = te
+    county_data["Wages and Salaries"] = ws
+
+    return county_data
+
+
+def get_percent_change(county_info: dict):
+    """
+    Calculates the percent change in each metric over the last 5 years.
+
+    This function iterates through the dictionary of county-level metrics and calculates
+    the percent change for each metric, comparing the most recent value to the value from
+    5 years ago. The result is returned in a dictionary where each key represents the metric,
+    and the value is a tuple containing:
+    - The current metric value
+    - The percent change compared to 5 years ago
+
+    Args:
+        county_info (dict): A dictionary containing county-level data (e.g., house value, rent rate, etc.).
+
+    Returns:
+        dict: A dictionary containing the percent change for each metric. The key is the metric name
+              (e.g., 'house_value'), and the value is a tuple with the current value and the percent
+              change over the last 5 years.
+    """
+    percent_change = {}
+    for key, value in county_info.items():
+        if value is not None:
+            key_name = key.lower().replace(' ', '_')
+            # (Current Metric, Change in Metric compared to 5 years)
+            percent_change[key_name] = (value.iloc[-1], ((value.iloc[-1] - value.iloc[-5]) / value.iloc[-5] * 100))
+    return percent_change
+
+
+def get_county_owner(state: str, county: str):
+    results = open_model("1")
+    place_name = f"{county}, Indiana"  # Temporal fix
+    results = results.loc[
+        (results["Place Name"] == place_name)
+    ]
+
+    if results.shape[0] == 0:
+        return None
+
+    return results
+
+
 if __name__ == "__main__":
+    results = get_county_owner("IN", "Adams County")
+    print(results["Year"])
     # counties = get_counties("indiana")
     # print(counties)
-    df = get_county_employment("IN", "Adams County")
-    for e in df:
-        print(e.head())
+    # house = get_county_house_value("IN", "Adams County")
+    # print(house)
+    # info = get_county_info("IN", "Adams County")
+    # change = get_percent_change(info)
+    # print(change)
+    # emp = get_county_employment("IN", "Adams County")
+    # print(emp[0].index)
